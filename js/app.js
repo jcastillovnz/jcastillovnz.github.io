6944859e86771a5c1caef7d5cdc862dd278bc0f2
Jose Castillo <k.y.258@hotmail.com>
2019-07-11 19:40:28 -0300

    UPDATE

---
+++ js/app.js
@@ -0,0 +1,11 @@
+/* particlesJS.load(@dom-id, @path-json, @callback (optional)); */
+particlesJS.load('particles-js', 'https://raw.githubusercontent.com/jcastillovnz/Cubik/master/js/particles.json', function() {
+  console.log('callback - particles.js config loaded');
+});
+
+
+
+
+
+
+
