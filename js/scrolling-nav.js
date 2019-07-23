2d3fa53b98694f7d3f3a7949f82c362ca4ce5258
Jose Castillo <k.y.258@hotmail.com>
2019-07-23 11:09:28 -0300

    update

--- js/scrolling-nav.js
+++ js/scrolling-nav.js
@@ -1,7 +1,7 @@
 
 $(document).ready(function(){
   // Add scrollspy to <body>
-  $('body').scrollspy({target: ".navbar", offset: 70});   
+  $('body').scrollspy({target: ".navbar", offset: 100});   
 
   // Add smooth scrolling on all links inside the navbar
   $("#myNavbar a").on('click', function(event) {
@@ -17,7 +17,7 @@ $(document).ready(function(){
       // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
       $('html, body').animate({
         scrollTop: $(hash).offset().top
-      }, 100, function(){
+      }, 2000, function(){
    
         // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
